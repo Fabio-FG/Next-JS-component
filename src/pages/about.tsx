d9d6a1f167a29206e6bc1116e383c55bf5df056b
fabioFG <fabio.fernando.guerreiro@gmail.com>
2023-02-23 14:53:00 +0000

    added an about route

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+export default function about() {
+  return (
+    <>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quis
+        aut ad, assumenda quo numquam odit non dolores sunt voluptates esse
+        nihil, incidunt minus nam nesciunt temporibus consequuntur ab beatae!
+      </p>
+    </>
+  );
+}
